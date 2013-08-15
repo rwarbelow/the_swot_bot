dee9bb86ef40b80824d4fd65a352f6f36c6438eb
Eric Allen <eric.justin.allen@gmail.com>
2013-08-15 10:12:28 -0500

    added attr_accessible to userIdentity model for mass assignment

--- app/models/user_identity.rb
+++ app/models/user_identity.rb
@@ -5,10 +5,13 @@ class UserIdentity < ActiveRecord::Base
 	validates :first_name, 						:presence => true
 	validates :last_name,  						:presence => true
 	
+	attr_accessible :username, :password, :password_confirmation, :first_name, :last_name
   attr_accessor :registration_code
 
 	has_secure_password
 
 	has_many sent_invitations, :class_name => "Invitation", :foreign_key => 'sender_id'
 	belongs_to :invitation
+
+
 end
