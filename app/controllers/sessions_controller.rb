614fc64223b844b94d6d5383c743da6465a04809
Eric Allen <eric.justin.allen@gmail.com>
2013-08-14 23:17:28 -0500

    added sessions controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,23 @@
+class SessionsController < ActionController::Base
+ 
+  def new
+  end
+
+  def create
+  	user = UserIdentity.find_by(:username => params[:user][:username])
+
+  	if user && user.authenticate(params[:user][:password])
+  		session[:id] = user.id
+  		flash[:notice] = "Successful Login!"
+  	else
+  		flash[:notice] = "Invalid Login... :("
+  	end
+  	redirect_to root_url
+  end
+  
+  def destroy
+  	session.clear
+  	flash[:notice] = "Logout Successful."
+  	redirect_to root_url
+  end
+end
