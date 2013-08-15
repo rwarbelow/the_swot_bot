a913d21d7afcc8b234ac10b5a5d533c68f9f5e82
jmyers0022 <jmyers0022@gmail.com>
2013-08-15 16:47:40 -0500

    implemented 909 error (redirect via before_filter)

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 SwotBot::Application.routes.draw do
   resources :user_identities
 
+  get '/error', to: 'home#909error', as: :error
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
