33a4b03a9a32a7f622ccf361aa60c5aef36b01da
Eric Allen <eric.justin.allen@gmail.com>
2013-08-15 21:08:52 -0500

    fixed typo

--- db/migrate/20130816013507_create_phone_numbers.rb
+++ db/migrate/20130816013507_create_phone_numbers.rb
@@ -3,7 +3,7 @@ class CreatePhoneNumbers < ActiveRecord::Migration
     create_table :phone_numbers do |t|
     	t.string :number 
     	t.string :type
-    	t.interger :phone_numberable_id
+    	t.integer :phone_numberable_id
     	t.string :phone_numberable_type
 
     	t.timestamps
