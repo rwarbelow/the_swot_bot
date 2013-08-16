4cace052b04a47edd7143d54cc5a432504fb0aa4
Allen Dayag <adayag@Allens-MacBook-Pro.local>
2013-08-16 11:03:49 -0500

    fixed user_identity spec tests

--- spec/Models/user_identity_spec.rb
+++ spec/Models/user_identity_spec.rb
@@ -2,8 +2,15 @@ require 'spec_helper'
 
 describe UserIdentity do
 	it { should validate_presence_of(:username) }
-	# it { should validate_uniqueness_of(:username) }
-	it { should validate_presence_of(:password_digest) }
+	it { should validate_presence_of(:password) }
 	it { should validate_presence_of(:first_name) }
-	it { should validate_presence_of(:last_name) }	
+	it { should validate_presence_of(:last_name) }
+	it { should validate_uniqueness_of(:username) }
+	it { should validate_uniqueness_of(:teacher_profile_id) }
+	it { should validate_uniqueness_of(:student_profile_id) }
+	it { should validate_uniqueness_of(:guardian_profile_id) }
+
+	it { should belong_to(:teacher_profile) }
+	it { should belong_to(:student_profile) }
+	it { should belong_to(:guardian_profile) }
 end
