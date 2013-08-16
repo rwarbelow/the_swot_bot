7c1d604205558416e763eb17d3751f40b4d7368f
Eric Allen <eric.justin.allen@gmail.com>
2013-08-15 20:42:59 -0500

    added phone numbers table

--- db/migrate/20130815175614_create_student_profiles.rb
+++ db/migrate/20130815175614_create_student_profiles.rb
@@ -5,10 +5,9 @@ class CreateStudentProfiles < ActiveRecord::Migration
   		t.string :gender, :null => false
   		t.date :birthday, :null => false
   		t.string :address
-  		t.integer :ccsd_id, :null => false
+  		t.string :ccsd_id, :null => false
   		t.integer :grade_level, :null => false
   		t.string :email
-  		t.integer :cell_phone_number
 
   		t.timestamps
   	end
