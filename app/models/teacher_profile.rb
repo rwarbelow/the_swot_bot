a6a875ffa9371d8017bf97e486c4388d55d905e1
Allen Dayag <adayag@Allens-MacBook-Pro.local>
2013-08-16 13:56:45 -0500

    updated existing model tests for guardian profile, teacher_profile, student_profile, and user_identity

--- app/models/teacher_profile.rb
+++ app/models/teacher_profile.rb
@@ -4,11 +4,11 @@ class TeacherProfile < ActiveRecord::Base
 	attr_accessible :title, :email
 
 	before_validation :downcase_email
-	
+
 	validates :title, :presence => true
 	validates :email, :presence => true, :uniqueness => true
 
-	has_many :phone_numbers, :as => :phone_numberable	
+	has_many :phone_numbers, :as => :phone_numberable
 	
 	private
 
--- spec/factories/teacher_profile.rb
+++ spec/factories/teacher_profile.rb
@@ -4,6 +4,5 @@ FactoryGirl.define do
   factory :teacher_profile do |f|
   	f.title { Faker::Lorem.word }
   	f.email { Faker::Internet.email }
-  	f.cell_phone_number { Faker::PhoneNumber.cell_phone }
   end
 end
