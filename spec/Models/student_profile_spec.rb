9ae4f443f79c15978c7c3b37318acdfd20138964
Eric Allen <eric.justin.allen@gmail.com>
2013-08-15 19:03:39 -0500

    uncommented tests and added factory

--- spec/Models/student_profile_spec.rb
+++ spec/Models/student_profile_spec.rb
@@ -1,9 +1,14 @@
 require 'spec_helper'
 
+
 describe StudentProfile do
+	before do
+	  user = FactoryGirl.create(:student_profile)
+	end
+
 	it { should validate_presence_of(:gender) }
 	it { should validate_presence_of(:birthday) }
 	it { should validate_presence_of(:ccsd_id) }
-	# it { should validate_uniqueness_of(:ccsd_id) }
+  it { should validate_uniqueness_of(:ccsd_id) }
 	it { should validate_presence_of(:grade_level) }	
 end
--- spec/Models/teacher_profile_spec.rb
+++ spec/Models/teacher_profile_spec.rb
@@ -2,9 +2,13 @@ require 'spec_helper'
 
 
 describe TeacherProfile do
+	before do
+	  user = FactoryGirl.create(:teacher_profile)
+	end
+
 	it { should validate_presence_of(:title) }
 	it { should validate_presence_of(:email) }
-	# it { should validate_uniqueness_of(:email) }
+	it { should validate_uniqueness_of(:email) }
 	it { should validate_presence_of(:cell_phone_number) }
-	# it { should validate_uniqueness_of(:cell_phone_number) }
+	it { should validate_uniqueness_of(:cell_phone_number) }
 end
