77d748793e69066967434768bf19a13e24157821
Allen Dayag <adayag@Allens-MacBook-Pro.local>
2013-08-16 13:57:38 -0500

    removed validations for uniqueness

--- spec/Models/user_identity_spec.rb
+++ spec/Models/user_identity_spec.rb
@@ -5,15 +5,12 @@ describe UserIdentity do
 	before do
 		FactoryGirl.create(:user_identity)
 	end
-	
+
 	it { should validate_presence_of(:username) }
 	it { should validate_presence_of(:password) }
 	it { should validate_presence_of(:first_name) }
 	it { should validate_presence_of(:last_name) }
 	it { should validate_uniqueness_of(:username) }
-	# it { should validate_uniqueness_of(:teacher_profile_id) }
-	# it { should validate_uniqueness_of(:student_profile_id) }
-	# it { should validate_uniqueness_of(:guardian_profile_id) }
 
 	it { should belong_to(:teacher_profile) }
 	it { should belong_to(:student_profile) }
