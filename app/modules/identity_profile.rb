27695f1f40b870b129fdb65445514212f8a7b161
Eric Allen <eric.justin.allen@gmail.com>
2013-08-15 21:12:04 -0500

    removed test method

--- app/modules/identity_profile.rb
+++ app/modules/identity_profile.rb
@@ -3,11 +3,6 @@ module IdentityProfile
 		includer.class_eval do
 			has_one	 :user_identity
 			delegate :first_name, :last_name, :username, :to => :random_user
-			# delegate :first_name, :last_name, :username, :to => :user_identity
 		end
-	end
-	
-	def random_user
-		UserIdentity.all.shuffle.first
 	end
 end
