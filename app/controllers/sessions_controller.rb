8e7dc0a74fa476135e2dac7e880b06b040151c9b
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-17 15:52:58 -0500

    fixed bugs in sessions controller and user identities controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,22 +7,35 @@ class SessionsController < ApplicationController
   end
 
   def create
-  	user = UserIdentity.where(:username => params[:user_identity][:username]).first
-  	if user && user.authenticate(params[:user_identity][:password])
-  		session[:user_id] = user.id
-      session[:guardian_profile_id] = user.guardian_profile_id
-      session[:teacher_profile_id] = user.teacher_profile_id
-      session[:student_profile_id] = user.student_profile_id
+  	@user = UserIdentity.where(:username => params[:user_identity][:username]).first
+  	if @user && @user.authenticate(params[:user_identity][:password])
+  		session[:user_id] = @user.id
+      session[:guardian_profile_id] = @user.guardian_profile_id
+      session[:teacher_profile_id] = @user.teacher_profile_id
+      session[:student_profile_id] = @user.student_profile_id
   		flash[:notice] = "Successful Login!"
+      redirect_to profile_path
   	else
   		flash[:notice] = "Invalid Login... :("
+      redirect_to root_url
     end
-  	redirect_to root_url
   end
   
   def destroy
   	session.clear
   	flash[:notice] = "Logout Successful."
   	redirect_to root_url
+  end
+
+  def profile_path
+    if @user.teacher_profile_id != nil
+      teacher_profile_path(@user.teacher_profile_id)
+    elsif @user.guardian_profile_id != nil
+      guardian_profile_path(@user.guardian_profile_id)
+    elsif @user.student_profile_id != nil
+      student_profile_path(@user.student_profile_id)
+    else
+      raise "Missing profile_id"
+    end
   end
 end
