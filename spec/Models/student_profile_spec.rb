1e6921d5427ed61803a392868f75583a7812ef28
Eric Allen <eric.justin.allen@gmail.com>
2013-08-18 10:44:26 -0500

    added student profile associations for classes, teacher, user_identity, assignments, submissions and actions

--- spec/Models/student_profile_spec.rb
+++ spec/Models/student_profile_spec.rb
@@ -11,7 +11,15 @@ describe StudentProfile do
 	it { should validate_presence_of(:ccsd_id) }
   it { should validate_uniqueness_of(:ccsd_id) }
 	it { should validate_presence_of(:grade_level) }
+
+	it { should have_one(:user_identity) }
 	it { should have_many(:guardianships) }
 	it { should have_many(:guardian_profiles) }
+	it { should have_many(:teachers) }
+	it { should have_many(:classes) }
+	it { should have_many(:assignments) }
+	it { should have_many(:submissions) }
+	it { should have_many(:actions) }
 	it { should have_many(:phone_numbers) }
+
 end
