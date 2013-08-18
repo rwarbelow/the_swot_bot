58f05dc355d9bda2aff544c463e29a2521b003cc
Eric Allen <eric.justin.allen@gmail.com>
2013-08-17 20:21:29 -0500

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130817223131) do
+ActiveRecord::Schema.define(:version => 20130818005959) do
+
+  create_table "assignments", :force => true do |t|
+    t.integer  "course_id"
+    t.string   "title"
+    t.text     "description"
+    t.datetime "created_at"
+  end
 
   create_table "courses", :force => true do |t|
     t.integer  "teacher_profile_id"
@@ -85,6 +92,11 @@ ActiveRecord::Schema.define(:version => 20130817223131) do
     t.datetime "updated_at",            :null => false
   end
 
+  create_table "readings", :force => true do |t|
+    t.integer  "message_id"
+    t.datetime "created_at"
+  end
+
   create_table "student_profiles", :force => true do |t|
     t.string   "gender",            :null => false
     t.date     "birthday",          :null => false
@@ -101,6 +113,13 @@ ActiveRecord::Schema.define(:version => 20130817223131) do
     t.string   "name"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+  end
+
+  create_table "submissions", :force => true do |t|
+    t.integer  "student_profile_id",                :null => false
+    t.integer  "assignment_id",                     :null => false
+    t.integer  "score",              :default => 0, :null => false
+    t.datetime "created_at"
   end
 
   create_table "teacher_profiles", :force => true do |t|
