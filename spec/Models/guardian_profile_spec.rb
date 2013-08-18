714ad9ca0068514d14b6adb22ba9a7b6409f4a52
Eric Allen <eric.justin.allen@gmail.com>
2013-08-18 11:19:51 -0500

    updated guardian  associations and tests

--- spec/Models/guardian_profile_spec.rb
+++ spec/Models/guardian_profile_spec.rb
@@ -2,7 +2,9 @@ require 'spec_helper'
 
 describe GuardianProfile do
   it { should validate_presence_of(:preferred_language) }
+  
+  it { should have_one(:user_identity) }
   it { should have_many(:guardianships) }
-  it { should have_many(:student_profiles) }
+  it { should have_many(:students) }
   it { should have_many(:phone_numbers) }
 end
