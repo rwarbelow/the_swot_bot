73fba0258ff9ebbd1c63b1ef5acbb43b4ee13f46
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-20 14:03:54 -0500

    built basic functionality for teachers to be able to enroll students in classes

--- app/controllers/teachers/enrollments_controller.rb
+++ app/controllers/teachers/enrollments_controller.rb
@@ -3,4 +3,16 @@ class Teachers::EnrollmentsController < Teachers::BaseController
   def index
     @students = Student.all
   end
+
+  def create
+    errors = []
+    @course = Course.find(params[:course_id])
+    params[:student_ids].each do |student_id|
+      enrollment = Enrollment.create(:student_id => student_id, :course_id => @course.id)
+      errors << "#{Student.find(student_id).first_name} #{Student.find(student_id).last_name} : #{enrollment.errors.full_messages.first}" unless enrollment.save
+    end
+    flash[:enrollment_errors] = errors
+    redirect_to teachers_course_path(@course)
+  end
+
 end
