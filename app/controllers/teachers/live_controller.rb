ff68f8a9d3d7cef2aad9faebf65b73eb75d99c87
jmyers0022 <jmyers0022@gmail.com>
2013-08-19 21:41:39 -0500

    cleaned up debugging calls in controller

--- app/controllers/teachers/live_controller.rb
+++ app/controllers/teachers/live_controller.rb
@@ -2,15 +2,11 @@ class Teachers::LiveController < Teachers::ApplicationController
 
   def classroom
     @course = Course.find(params[:course_id])
-    p params[:course_id]
     @students = @course.students.all
   end
 
-  def new_action
-  end
-
   def create_action
-    action = StudentActionType.find_by_name(params[:action_name]) #change in js
+    action = StudentActionType.find_by_name(params[:action_name])
     course_id = params[:course_id]
     student_ids = params[:student_ids]
 
@@ -21,6 +17,7 @@ class Teachers::LiveController < Teachers::ApplicationController
     save_action(action, student_ids, course_id) if student_ids
     
     save_attendance(absent_students, tardy_students, present_students, course_id) if present_students
+
     render :json => "SUCCESS!"
   end
 end
