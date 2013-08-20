b58030293ccd1c82bf8a6044b9574f04ab9f10b0
jmyers0022 <jmyers0022@gmail.com>
2013-08-20 18:20:08 -0500

    added attr_accessible for allow_multiple_entries

--- app/models/student_action_category.rb
+++ app/models/student_action_category.rb
@@ -1,5 +1,5 @@
 class StudentActionCategory < ActiveRecord::Base
-  attr_accessible :name, :student_action_type_id
+  attr_accessible :name, :student_action_type_id, :allow_multiple_entries_per_date
 	validates :name, :presence => true
 
 	has_many :student_action_types
