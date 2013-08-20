5c016a9bf4915d597e2f4086564b2ac58b2d3e63
jmyers0022 <jmyers0022@gmail.com>
2013-08-20 18:15:17 -0500

    [wip] setup ajax posting for activities via json (TODOs in comments

--- app/controllers/teachers/base_controller.rb
+++ app/controllers/teachers/base_controller.rb
@@ -1,13 +1,71 @@
 class Teachers::BaseController < ApplicationController
 	before_filter :require_teacher
 
+  #TODO: Move this into model & refactor
+  def student_attendance(students)
+    on_time, tardy, absent = [], [], []
+
+    students.each do |student|
+      on_time << student.student_actions.where(student_action_type_id: 1, date: Date.today).first
+      tardy << student.student_actions.where(student_action_type_id: 2, date: Date.today).first
+      absent << student.student_actions.where(student_action_type_id: 3, date: Date.today).first
+    end
+
+    attendance = {}
+    on_time_ids, tardy_ids, absent_ids = [], [], []
+
+    on_time.compact.each do |student_action|
+      on_time_ids << student_action.enrollment.student_id
+    end
+
+    tardy.compact.each do |student_action|
+      tardy_ids << student_action.enrollment.student_id
+    end
+
+    absent.compact.each do |student_action|
+      absent_ids << student_action.enrollment.student_id
+    end
+
+    attendance[:on_time] = on_time_ids
+    attendance[:tardy] = tardy_ids
+    attendance[:absent] = absent_ids
+    attendance
+  end
+
+  #TODO: Move this into model
   def save_action(action, student_ids, course_id)
-    student_ids.each do |student_id|
-      action_name = StudentActionType.find_by_name(action.to_s)
-      student = Student.find(student_id.to_i)
-      enrolled = student.enrollments.where(course_id: course_id).first
-      new_record = StudentAction.new(date: Date.today, enrollment_id: enrolled.id, student_action_type_id: action_name.id)
-      new_record.save
+
+    if student_ids
+      student_ids.each do |student_id|
+
+        action_type = StudentActionType.find_by_name(action.to_s)
+        student = Student.find(student_id.to_i)
+        enrollment = student.enrollments.find_by_course_id(course_id)
+        
+        # Idea of what to call when code is moved into the model
+        # student_action = StudentAction.new(enrollment_id: enrollment.id, 
+        #                                    date: Date.today, 
+        #                                    action_type_id: action_type.id)
+        # student_action.save_or_update!
+        if !action_type.category.allow_multiple_entries_per_date?
+          # find the preexising database row for the existing action of this 
+          # same family and update its action_type_id
+          student_action_of_same_family = StudentAction.where(
+                                                          enrollment_id: enrollment.id, 
+                                                          date: Date.today, 
+                                                          student_action_type_id: [action_type.category.student_action_types.map(&:id)]
+                                                        ).first
+
+          if student_action_of_same_family
+            student_action_of_same_family.update_attribute(:student_action_type_id, action_type.id)
+            next
+          end
+        end
+        
+        student_action = StudentAction.create(enrollment_id: enrollment.id,
+                                              date: Date.today,
+                                              student_action_type_id: action_type.id)
+      end
     end
   end
 
