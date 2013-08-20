c14a8004505493960c4f8e2ed5634380c79c0df5
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-20 14:03:12 -0500

    built out basic functionality for adding assignments

--- app/controllers/teachers/assignments_controller.rb
+++ app/controllers/teachers/assignments_controller.rb
@@ -4,4 +4,18 @@ class Teachers::AssignmentsController < Teachers::BaseController
     @course = Course.find(params[:course_id])
     @assignments = @course.assignments
   end
+
+  def create
+    @course = Course.find(params[:course_id])
+    @assignment = Assignment.new(params[:assignment])
+    @assignment.course_id = @course.id
+    if @assignment.save
+      redirect_to teachers_course_assignments_path(@course)
+    else
+      flash[:assignment_errors] = @assignment.errors.full_messages
+      @assignments = @course.assignments
+      render 'teachers/assignments/index'
+    end
+  end
+
 end
--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -1,11 +1,17 @@
 class Assignment < ActiveRecord::Base
-  attr_accessible :course_id, :title, :description
+  attr_accessible :course_id, :title, :description, :due_date
 
 	validates :course_id, :presence => true
 	validates :title, :presence => true
 	validates :maximum_points, :presence => true
 	validates :due_date, :presence => true
+  validate :valid_date
 
 	belongs_to :course
 	has_many 	 :submissions
+
+def valid_date
+  errors.add(:due_date, "date cannot be before 8-25-13") if due_date && due_date < Date.parse("2013-08-25")
+end
+
 end
