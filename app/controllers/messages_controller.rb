3896938540524f1377a4d828420428fd798308da
Rachel Warbelow <rachelwarbelow@gmail.com>
2013-08-21 16:44:57 -0500

    finished student views and messaging functionality for students

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,11 +2,17 @@ class MessagesController < ApplicationController
 
   def new
     @message = Message.new
-    @teachers = Teacher.all
+    if current_guardian?
+      @recipients = Teacher.all
+    elsif current_student?
+      @recipients = Teacher.all
+    elsif current_teacher?
+      @recipients = Teacher.all + Guardian.all + Student.all
+    end
   end
 
   def create
-  	@message = Message.create(:author_id => current_user.id, :target_id => (Identity.find_by_teacher_id(params[:message][:to]).id), :body => params[:message][:body])
+  	@message = Message.create(:author_id => current_user.id, :target_id => (Identity.find_by_teacher_id(params[:message][:to]).id), :body => params[:message][:body], :subject => params[:message][:subject])
   	if @message.save
       redirect_to messages_path
     else
@@ -16,6 +22,7 @@ class MessagesController < ApplicationController
   end
 
   def index
+    @message = Message.new
     @received_messages = Message.where(:target_id => current_user.id)
     @sent_messages = Message.where(:author_id => current_user.id)
   end
