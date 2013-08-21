ae0a265043f9ea576fdb2f14f272da51695a2584
Rachel Warbelow <rachelwarbelow@gmail.com>
2013-08-21 15:00:30 -0500

    messaging functionality

--- app/controllers/students/courses_controller.rb
+++ app/controllers/students/courses_controller.rb
@@ -3,10 +3,14 @@ class Students::CoursesController < Students::BaseController
   before_filter :must_be_a_student, :only => [:index]
 
   def index
+    @student = current_student
     @courses = current_student.courses
   end
 
   def show
+    @assignments = Assignment.where(:course_id => params[:id])
+    enrollment = Enrollment.where(:student_id => current_student.id, :course_id => params[:id]).first
+    @actions = StudentAction.where(:enrollment_id => enrollment.id)
   end
 
   private
