f99094d9d5f83b74c9e6d6409fb45d9b4773ddf8
Eric Allen <eric.justin.allen@gmail.com>
2013-08-21 10:43:26 -0500

    added controller and model for sms functionality

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -3,10 +3,6 @@ class CallbacksController < ApplicationController
 	def cloud_elements
 	  from = params['From']
 	  ccsd_id = params['Body'].strip
-	  puts from 
-	  puts ccsd_id
-	  puts "*" * 80
-	  puts params
 
 	  message = Sms.new(from, ccsd_id)
 		message.initiate_response
