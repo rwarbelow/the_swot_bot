3429aebed8fc00631783ae975f5899da89cf897c
adayag <a.dayag@gmail.com>
2013-08-22 11:32:14 -0500

    added security settings

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -46,7 +46,7 @@ class ApplicationController < ActionController::Base
         student_actions_array << course.enrollments.where(student_id:student.id).first.student_actions.current_week_report.group_by {|action| action.student_action_type.name}
       end
      report.start_new_page do |page|
-        page.item(:student_name).value(student.first_name)
+        page.item(:student_name).value(student.full_name)
         page.item(:ccsd_id).value(student.ccsd_id)
         page.item(:start_date).value(start_date)
         page.item(:end_date).value(end_date)
@@ -71,6 +71,12 @@ class ApplicationController < ActionController::Base
       end
     end
     if password = true
+      security_settings = {:user_password  => 'foo',
+                           :owner_password => :random,
+                              :permissions => { :print_document  => true,
+                                                :modify_contents => false,
+                                                :copy_contents   => true}}
+
       send_data report.generate(filename: 'swot_report.pdf',security: security_settings)
     else
       send_data report.generate(filename: 'swot_report.pdf')
