ed86dafc6efb6f7ce8c3f71db5051ffc4c1b58e2
Rachel Warbelow <rachelwarbelow@gmail.com>
2013-08-31 19:22:15 -0700

    deleted test code

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -1,41 +1,41 @@
 class Assignment < ActiveRecord::Base
-  attr_accessible :course_id, :title, :description, :due_date, :maximum_points
+  attr_accessible :course_id, :title, :description, :due_date, :maximum_points, :assignment_category_id
 
 	validates :course_id, :presence => true
 	validates :title, :presence => true
 	validates :maximum_points, :presence => true
 	validates :due_date, :presence => true
-	validates :category_id, :presence => true
+	validates :assignment_category_id, :presence => true
 
 	belongs_to :course
 	belongs_to :assignment_category
 	has_many 	 :submissions
 
-def valid_date
-  errors.add(:due_date, "Date must be for current school year") if due_date && due_date < Date.parse("2013-08-25")
-end
+	def valid_date
+	  errors.add(:due_date, "Date must be for current school year") if due_date && due_date < Date.parse("2013-08-25")
+	end
 
-def calculate_percent(earned)
-	(earned.to_f / maximum_points.to_f) * 100
-end
+	def calculate_percent(earned)
+		(earned.to_f / maximum_points.to_f)
+	end
 
 	def calculate_grade(earned)
 		if calculate_percent(earned)
 			case calculate_percent(earned)
-			when 90..100
+			when 0.90..1
 				return "A"
-			when 80..89
+			when 0.80..0.899
 				return "B"
-			when 70..79
+			when 0.70..0.799
 				return "C"
-			when 60..69
+			when 0.60..0.699
 				return "D"
-			when 0..59
+			when 0..0.599
 				return "F"
 			else
 				return ""
 			end
 		end
 	end
-
+	
 end
