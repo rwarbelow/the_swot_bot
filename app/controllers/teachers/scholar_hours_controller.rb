0816cf5c4a03df270a9688df1d17a5dbbe085ad1
Rachel Warbelow <rachelwarbelow@gmail.com>
2013-09-01 16:22:49 -0700

    added controller for scholar hours

---
+++ app/controllers/teachers/scholar_hours_controller.rb
@@ -0,0 +1,35 @@
+class Teachers::ScholarHoursController < Teachers::BaseController
+
+  def index
+  	@new_scholar_hours = ScholarHour.where(date_assigned: Date.today, status: "Not Complete")
+  	@old_scholar_hours = ScholarHour.where(status: "Not Complete") - @new_scholar_hours
+  	@completed_scholar_hours = ScholarHour.where(status: "Complete")
+  	p "*"*80
+  	p @completed_scholar_hours
+  end
+
+  def generate_scholar_hour_list
+  	@student_ids = ScholarHour.find_daily_student_list
+  	@student_ids.each do |student|
+  		id = student[0]
+  		reason = student[1]
+  		ScholarHour.create(:student_id => id, :reason => reason, :date_assigned => Date.today)
+  	end
+  	redirect_to teachers_scholar_hours_path
+  end
+
+  def destroy
+  	@scholarhour = ScholarHour.find(params[:id])
+  	@scholarhour.delete
+  	redirect_to teachers_scholar_hours_path
+  end
+
+  def update
+  	@scholarhour = ScholarHour.find(params[:id])
+  	@scholarhour.date_served = Date.today
+  	@scholarhour.status = "Complete"
+  	@scholarhour.save
+  	redirect_to teachers_scholar_hours_path
+  end
+
+end
