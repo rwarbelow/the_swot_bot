11ca647618c301700fab8fe340104a52e5d095ad
Rachel Warbelow <rachelwarbelow@gmail.com>
2013-09-02 19:28:08 -0700

    added timezone config

--- config/application.rb
+++ config/application.rb
@@ -42,7 +42,7 @@ module SwotBot
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
-
+    config.time_zone = 'Pacific Time (US & Canada)'
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
 
