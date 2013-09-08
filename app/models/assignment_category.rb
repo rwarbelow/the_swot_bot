1a7a905be5d49debf81005a26cef42f0f03bd7d4
Rachel Warbelow <rachelwarbelow@gmail.com>
2013-09-07 17:49:32 -0700

    removed unneeded method

--- app/models/assignment_category.rb
+++ app/models/assignment_category.rb
@@ -7,25 +7,18 @@ class AssignmentCategory < ActiveRecord::Base
 	has_many :submissions, :through => :assignments
 	belongs_to :course
 
-
- 	def worth(student)
- 		@worth = 0
- 		if submissions.where('student_id = ?', student.id).length > 0
-	 		submissions.where('student_id = ?', student.id).each do |submission|
-	 			@worth += (submission.assignment.maximum_points.to_f) if submission.assignment.due_date < Date.today
- 		end
- 		end
- 		@worth
- 	end
-
- 	def earned(student)
- 		@earned = 0
- 		if submissions.where('student_id = ?', student.id).length > 0
-	 		submissions.where('student_id = ?', student.id).each do |submission|
-	 			@earned += (submission.points_earned.to_f) if submission.assignment.due_date < Date.today
-	 		end
-	 	end
- 		@earned
- 	end
+  def self.calculate_category_points(submissions_hash)
+		@categories_hash = Hash.new([])
+  	submissions_hash.each do |category|
+  		@total_earned = 0
+  		@total_worth = 0
+  		category[1].each do |score_pair|
+  			@total_earned += score_pair[0]
+  			@total_worth += score_pair[1]
+	  	end
+	  	@categories_hash[category[0]] = [@total_earned, @total_worth]
+	  end
+	  @categories_hash
+	end
  	
 end
