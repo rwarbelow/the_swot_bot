baff71188a2a7601d620cb305983096b91fd2bfb
Rachel Warbelow <rachelwarbelow@gmail.com>
2013-09-12 18:15:47 -0700

    removed date requirement to calculate student grades

--- app/controllers/teachers/submissions_controller.rb
+++ app/controllers/teachers/submissions_controller.rb
@@ -8,16 +8,19 @@ class Teachers::SubmissionsController < Teachers::BaseController
     @assignment = Assignment.find(params[:assignment_id])
     course = Course.find(@assignment.course_id)
     ids_scores = params[:students].zip(params[:scores])
+    p ids_scores
     ids_scores.each do |score|
       student = Student.find(score[0])
       points = score[1]
-      submission = Submission.find_or_create_by_assignment_id_and_student_id(@assignment.id, student.id)
-      submission.points_earned = points
-      submission.save
-      enrollment = Enrollment.find_by_course_id_and_student_id(@assignment.course_id, student.id)
-      enrollment.current_grade = student.calculate_percent(course)
-      enrollment.save
-      errors << "#{Student.find(score[0]).first_name} #{Student.find(score[0]).last_name} : #{submission.errors.full_messages.first}" unless submission.save
+      if score[1] != ""
+        submission = Submission.find_or_create_by_assignment_id_and_student_id(@assignment.id, student.id)
+        submission.points_earned = points
+        submission.save
+        enrollment = Enrollment.find_by_course_id_and_student_id(@assignment.course_id, student.id)
+        enrollment.current_grade = student.calculate_percent(course)
+        enrollment.save
+        errors << "#{Student.find(score[0]).first_name} #{Student.find(score[0]).last_name} : #{submission.errors.full_messages.first}" unless submission.save
+      end
     end
     if errors.length > 0
       flash[:submission_errors] = errors
