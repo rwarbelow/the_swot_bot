221737a296103463d2bd1f077918ac8163eb2a21
Rachel Warbelow <rachelwarbelow@gmail.com>
2013-09-19 23:53:13 -0700

    added methods to sort daily summary by bank balance or student last name

--- app/controllers/teachers/dashboard_controller.rb
+++ app/controllers/teachers/dashboard_controller.rb
@@ -1,9 +1,24 @@
 class Teachers::DashboardController < Teachers::BaseController
+	helper_method :sort_column
   def index
   end
 
   def daily_summary
-  	@students = Student.includes(:identity, :student_actions).all.sort!{ |a, b| a.last_name <=> b.last_name}
+  	if params[:sort] == "Name" 
+	  	@students = Student.includes(:identity, :student_actions).all.sort!{ |a, b| a.last_name <=> b.last_name}
+	  elsif params[:sort]
+	  	@students = Student.order(sort_column + " " + "desc")
+	  else
+	  	@students = Student.includes(:identity, :student_actions).all.sort!{ |a, b| a.last_name <=> b.last_name}
+		end
+
   end
+
+  private
+  
+  def sort_column
+    Student.column_names.include?(params[:sort]) ? params[:sort] : "id"
+  end
+
 
 end
