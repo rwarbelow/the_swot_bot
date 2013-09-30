0b025aa73782f70f41068fc4278c8d762eaf4242
Eric Allen <eric.justin.allen@gmail.com>
2013-09-30 12:45:55 -0600

    Solved the N+1 Query problem for `SmsBroadcaster#broadcast`
    Dropped number of queries per student from two to one.

--- lib/assets/sms_broadcaster.rb
+++ lib/assets/sms_broadcaster.rb
@@ -1,13 +1,8 @@
 module SmsBroadcaster
   def self.broadcast(students, text_body)
-    guardian_numbers = students.map do |student|
-      student.guardians.map do |guardian|
-        guardian.textable_numbers.map do |phone|
-          "+1" + phone.number
-        end
-      end
-    end.flatten
-
+    guardians = Guardian.includes(:phone_numbers, :students).where(students: {id: students.map(&:id)}, phone_numbers: {kind: 'cell'})
+    guardian_numbers = guardians.map {|guardian| guardian.phone_numbers.map(&:number)}.flatten
+    
     TextMessage.sendMessage(to: guardian_numbers,
                             message: text_body)
   end
