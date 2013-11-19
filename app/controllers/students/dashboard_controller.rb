a1de342818dff314ccd5ce10567317c6618013b9
Rachel Warbelow <rachelwarbelow@gmail.com>
2013-11-18 22:47:53 -0800

    added image for viewing swot report and facebook share

--- app/controllers/students/dashboard_controller.rb
+++ app/controllers/students/dashboard_controller.rb
@@ -1,6 +1,7 @@
 class Students::DashboardController < Students::BaseController
   def index
   	@student = current_student
+    @bonuses = @student.positive(@student.collect_student_actions)
   	@incompleted_goals = Goal.all.select{ |goal| goal.student_id == current_student.id && goal.status == "In Progress"}
   	@completed_goals = Goal.all.select{ |goal| goal.student_id == current_student.id && goal.status == "Complete"}
     @incomplete_scholar_hours = ScholarHour.where(student_id: current_student.id).where(status: "Not Complete")
