f597cf0740301f3e206d2793f91a78d235c1c5e4
adayag <a.dayag@gmail.com>
2013-11-27 12:18:04 -0600

    change report day to current day as quick fix for short school week report print out

--- app/models/student_action.rb
+++ app/models/student_action.rb
@@ -10,14 +10,8 @@ class StudentAction < ActiveRecord::Base
   before_save :set_default_date
   after_create :update_bank_balance
 
-  # e.g.
-  #  from = Date.parse '2013-01-01'
-  #  to   = Date.parse '2014-01-01'
-  #  (from...to).step(7) do |date|
-  #    puts StudentAction.week_report date
-  #  end
   def self.week_report(from=Date.current)
-  	if from.friday? || from.saturday? || from.sunday?
+  	if from.wednesday? || from.friday? || from.saturday? || from.sunday?
       current_week_report()
     else
     	last_week_report()
@@ -30,23 +24,6 @@ class StudentAction < ActiveRecord::Base
     student.save
   end
 
-  # maybe something like this:
-  # def self.for_week(n=0)
-  #   week = Date.current.cweek + n
-  #   year = Date.current.year
-  #   while week < 0
-  #     year -= 1
-  #     week += 52
-  #   end
-  #   start_date = start_date = Date.commercial(year, week, 1)
-  #   end_date   = start_date + 7
-	# 	where("student_actions.created_at between ? and ?", start_date, end_date)
-  # end
-  #
-  # would call with like StudentAction.by_week(-1).report
-  # insetad of           StudentAction.previous_week.report
-
-  # rename -> current_week
 	def self.current_week_report(start_week = Date.commercial(Date.today.year, Date.today.cweek, 1), end_week = Date.commercial(Date.today.year, Date.today.cweek, 6))
 		where("student_actions.created_at between ? and ?", start_week, end_week)
 	end
