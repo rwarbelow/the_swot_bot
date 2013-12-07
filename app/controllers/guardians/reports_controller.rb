41a6bff5aa37a3e9178947b920317e33bfae0d13
Rachel Warbelow <rachelwarbelow@gmail.com>
2013-12-06 20:05:38 -0800

    removed password requirement

--- app/controllers/guardians/reports_controller.rb
+++ app/controllers/guardians/reports_controller.rb
@@ -2,6 +2,6 @@ class Guardians::ReportsController < Guardians::BaseController
 	def student_report
 		students = Student.where(:ccsd_id => params[:ccsd_id])
 
-		new_report(students)
+		new_report(students, password = false)
 	end
 end
